fad1c24b0b33690087d52329330f4719b8a67408
Ashlin Aronin & Alex Brown <student@epicodus.com>
2015-09-16 10:08:46 -0700

    Write specs for Player and Turn and make them fail

---
+++ spec/specs.js
@@ -0,0 +1,38 @@
+describe('Player', function() {
+  it("creates a player with the given name", function() {
+    var testPlayer = new Player("Mrs. Piggy");
+    expect(testPlayer.playerName()).to.equal("Mrs. Piggy");
+  });
+
+  // current turn
+
+  // take turn
+});
+
+describe('Turn', function() {
+  it("creates a new turn with a default score of zero", function() {
+    var testPlayer = new Player("Porker Posey");
+    var testTurn = new Turn(testPlayer);
+    expect(testTurn.currentScore()).to.equal(0);
+  });
+});
+
+describe('Turn.roll()', function() {
+  it("it produces a random number between 1 and 6 and adds it to the currentScore", function() {
+    var testPlayer = new Player("Porker Posey");
+    var testTurn = new Turn(testPlayer);
+    testTurn.roll();
+    expect(testTurn.currentScore()).to.be.within(1,6);
+  });
+});
+
+describe('Turn.pass()', function() {
+  it("ends the current turn and adds currentScore to player.totalScore", function() {
+    var testPlayer = new Player("Porker Posey");
+    var testTurn = new Turn(testPlayer);
+    testTurn.roll();
+    var scoreAfterRoll = testTurn.currentScore;
+    testTurn.pass();
+    expect(testPlayer.totalScore).to.equal(scoreAfterRoll);
+  });
+});
